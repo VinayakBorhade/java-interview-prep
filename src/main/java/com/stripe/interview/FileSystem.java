d9627e2916f6b70ffd9c8642703f4522ee33772b
Vinayak Borhade <vinayakborhade@gmail.com>
2025-10-31 17:48:24 +0530

    implementing concurrent file system get and pull calls using java futures and ES

--- src/main/java/com/stripe/interview/FileSystem.java
+++ src/main/java/com/stripe/interview/FileSystem.java
@@ -2,6 +2,8 @@ package com.stripe.interview;
 
 import java.lang.*;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 
 
 class Node {
@@ -19,6 +21,18 @@ class FileSystem {
 
     public FileSystem() {
         root = new Node();
+    }
+
+    private void sleep(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Date ts() {
+        return new Date();
     }
 
     private boolean createPathUtil(String path, Node root, String[] parts, Integer value) {
@@ -42,29 +56,42 @@ class FileSystem {
         return true;
     }
 
-    public boolean createPath(String path, int value) {
-        String[] parts = path.split("/");
+    public CompletableFuture<Boolean> createPath(long delay, ExecutorService es, String path, int value) {
+        return CompletableFuture.supplyAsync(() -> {
+            sleep(delay);
+            return createPath(path, value);
+        }, es);
+    }
 
+    public Boolean createPath(String path, int value) {
+        System.out.printf(" [%s] Executing createPath with path = %s and value = %s \n", ts(), path, value);
+        String[] parts = path.split("/");
         // System.out.println(" parts : " + parts[1] + " len " + parts.length);
         return createPathUtil(path, root, parts, value);
     }
 
-    public int get(String path) {
+    public CompletableFuture<Integer> get(long delay, ExecutorService es, String path) {
+        return CompletableFuture.supplyAsync(() -> {
+            sleep(delay);
+            return get(path);
+        }, es);
+    }
+
+    public Integer get(String path) {
+        System.out.printf(" [%s] Executing get with path = %s \n", ts(), path);
         String[] parts = path.split("/");
         Node n_it = root;
-        for(int i = 1; i < parts.length; i++) {
+        for (int i = 1; i < parts.length; i++) {
             String part = parts[i];
 
             // System.out.println(" in get(): part = " + part);
 
             n_it = n_it.children.getOrDefault(part, null);
-            if(n_it == null) {
+            if (n_it == null) {
                 break;
             }
         }
-
-
-        if(n_it == null || n_it.value == null) {
+        if (n_it == null || n_it.value == null) {
             return -1;
         }
         return n_it.value;
--- src/test/java/com/stripe/interview/FileSystemTest.java
+++ src/test/java/com/stripe/interview/FileSystemTest.java
@@ -1,21 +1,72 @@
 package com.stripe.interview;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.*;
 
 public class FileSystemTest {
 
-    FileSystem fs = new FileSystem();
+    FileSystem underTest;
+
+    @Before
+    public void setup() {
+        underTest = new FileSystem();
+    }
 
     /**
      *      ["FileSystem","createPath","get"]
      *      [[],["/a",1],["/a"]]
      */
     @Test
-    public void test_happyCase() {
-        assertTrue(fs.createPath("/a", 1));
-        assertEquals(1, fs.get("/a"));
+    public void test_happyCase() throws ExecutionException, InterruptedException {
+        assertTrue(underTest.createPath("/a", 1));
+        assertEquals(1, underTest.get("/a").intValue());
+    }
+
+    @Test
+    public void test_multipleThreads() throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(5);
+        CompletableFuture<Boolean> cf1 = underTest.createPath(100, es, "/abc", 1);
+        CompletableFuture<Boolean> cf2 = underTest.createPath(500, es, "/abc/def", 1);
+        CompletableFuture<Boolean> cf3 = underTest.createPath(2000, es, "/abc/def/pqr", 1);
+        assertTrue(cf1.get());
+        assertTrue(cf2.get());
+        assertTrue(cf3.get());
+    }
+
+    @Test
+    public void test_multipleThreads_whenExecutedOutOfOrder_thenReturnFalse() throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(5);
+        CompletableFuture<Boolean> cf1 = underTest.createPath(5000, es, "/abc", 1);
+        CompletableFuture<Boolean> cf2 = underTest.createPath(100, es, "/abc/def", 1);
+        CompletableFuture<Boolean> cf3 = underTest.createPath(500, es, "/abc/def/pqr", 1);
+        assertTrue(cf1.get());
+        assertFalse(cf2.get());
+        assertFalse(cf3.get());
+    }
+
+    @Test
+    public void test_multipleThreads_whenExecutionChained_thenReturnTrue() throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(5);
+//        CompletableFuture<Boolean> cf1 = underTest.createPath(300, es, "/abc", 1);
+//        CompletableFuture<Boolean> cf2 = underTest.createPath(200, es, "/abc/def", 1);
+//        CompletableFuture<Boolean> cf3 = underTest.createPath(100, es, "/abc/def/pqr", 1);
+
+        assertTrue(underTest.createPath(300, es, "/abc", 1)
+                .thenCompose(res -> {
+                    assertTrue(res);
+                    return underTest.createPath(200, es, "/abc/def", 1);
+                })
+                .thenCompose(res -> {
+                    assertTrue(res);
+                    return underTest.createPath(100, es, "/abc/def/pqr", 1);
+                }).get());
+
     }
 }
